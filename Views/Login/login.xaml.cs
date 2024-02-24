c7bc9eb51bb8c6e044af57a482a1fd5b467eb498
Aleksx07 <134466359+Aleksx07@users.noreply.github.com>
2024-02-23 18:48:16 -0600

    Cambio en el login

--- Views/Login/login.xaml.cs
+++ Views/Login/login.xaml.cs
@@ -1,24 +1,41 @@
-namespace ProyectoFinal_Grupo3_Floristeria_Margaritas.Views.Login;
-
-public partial class login : ContentPage
+namespace ProyectoFinal_Grupo3_Floristeria_Margaritas.Views.Login
 {
-    public login()
+    public partial class login : ContentPage
     {
-        InitializeComponent();
-    }
+        private Color originalBackgroundColor;
 
-    private void btnEntrar_Clicked(object sender, EventArgs e)
-    {
+        public login()
+        {
+            InitializeComponent();
 
-    }
+        
+        }
 
-    private void btnRegistrarse_Clicked(object sender, EventArgs e)
-    {
+        private async void btnEntrar_Clicked(object sender, EventArgs e)
+        {
 
-    }
+        }
 
-    private void btnAplicar_Clicked(object sender, EventArgs e)
-    {
+        private async void btnRegistrarse_Clicked(object sender, EventArgs e)
+        {
+            // Aquí puedes navegar a la página deseada
+            await Navigation.PushAsync(new Views.Login.singin());
+        }
 
+
+        private void btnAplicar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void LabelRecuperar_Tapped(object sender, System.EventArgs e)
+        {
+            // Animación de escala al hacer clic en el Label
+            await labelRecuperar.ScaleTo(0.8, 50, Easing.Linear);
+            await labelRecuperar.ScaleTo(1, 50, Easing.Linear);
+
+            // Aquí puedes navegar a la página deseada
+            await Navigation.PushAsync(new Views.Login.RestaurarContra());
+        }
     }
 }
