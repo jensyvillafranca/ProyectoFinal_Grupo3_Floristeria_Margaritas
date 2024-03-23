b40ba245687d629d7be35802139f825ae53f9c0d
Gustavo <tavo198704@gmail.com>
2024-03-23 11:00:53 -0600

    Temp

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -3,6 +3,10 @@ using Microsoft.Maui;
 using Microsoft.Maui.Controls.Compatibility;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
+using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.Crashlytics;
+using Plugin.Firebase.Core.Platforms.Android;
 
 namespace ProyectoFinal_Grupo3_Floristeria_Margaritas
 {
@@ -29,6 +33,30 @@ namespace ProyectoFinal_Grupo3_Floristeria_Margaritas
 #endif
 
             return builder.Build();
+        }
+
+        public static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+        {
+            builder.ConfigureLifecycleEvents(events =>
+            {
+#if IOS
+                events.AddiOS(iOS => iOS.FinishedLaunching((app, launchOptions) => {
+                CrossFirebase.Initialize(CreateCrossFirebaseSettings());
+                }));
+#else
+                events.AddAndroid(android => android.OnCreate((activity, _) =>
+                CrossFirebase.Initialize(activity, CreateCrossFirebaseSettings())));
+
+#endif
+            });
+
+            builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+            return builder;
+        }
+
+        private static CrossFirebaseSettings CreateCrossFirebaseSettings()
+        {
+            return new CrossFirebaseSettings(isAuthEnabled: true);
         }
     }
 }
