857baa63f5a200caa0e45338d9d14e6a8b4b1cf9
Gustavo <tavo198704@gmail.com>
2024-03-24 20:31:07 -0600

    Historia de Pedidos - Repartidor

---
+++ Modelos/historialEntregasModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Grupo3_Floristeria_Margaritas.Modelos
+{
+    public class historialEntregasModel
+    {
+        public int idpedido {  get; set; }
+        public DateTime fechapedido { get; set; }
+        public string? nombrescliente { get; set; }
+        public double ganancia { get; set; }
+        public int idestadopedido { get; set; }
+        public int calificacion { get; set; } = 0;
+        public string? motivocalificacion { get; set; }
+    }
+}
--- Views/PantallasRepartidor/historialEntregas.xaml.cs
+++ Views/PantallasRepartidor/historialEntregas.xaml.cs
@@ -1,57 +1,199 @@
+using Java.Time;
+using Org.Apache.Http.Cookies;
+using Plugin.Firebase.Firestore.Platforms.Android.Extensions;
+using ProyectoFinal_Grupo3_Floristeria_Margaritas.Controllers;
+using ProyectoFinal_Grupo3_Floristeria_Margaritas.Modelos;
+using ProyectoFinal_Grupo3_Floristeria_Margaritas.ViewModel;
+using ProyectoFinal_Grupo3_Floristeria_Margaritas.Views.Productos;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using static ProyectoFinal_Grupo3_Floristeria_Margaritas.Views.Productos.productos;
 
 namespace ProyectoFinal_Grupo3_Floristeria_Margaritas.Views.PantallasRepartidor;
 
 public partial class historialEntregas : ContentPage
 {
-    public ObservableCollection<EntregaItem> Entregas { get; set; }
+    private ApiService _apiService = new ApiService();
+    public ObservableCollection<historialEntregasViewModel> Historiales { get; set; }
     ObservableCollection<string> filtros;
+
+    private string _searchQuery;
+    public string SearchQuery
+    {
+        get { return _searchQuery; }
+        set
+        {
+            if (_searchQuery != value)
+            {
+                _searchQuery = value;
+                OnPropertyChanged(nameof(SearchQuery));
+                UpdateFilteredItems();
+            }
+        }
+    }
+
+    private int _selectedFilter = -1;
+    public int SelectedFilter
+    {
+        get { return _selectedFilter; }
+        set
+        {
+            if (_selectedFilter != value)
+            {
+                _selectedFilter = value;
+                OnPropertyChanged(nameof(SelectedFilter));
+                UpdateFilteredItems();
+            }
+        }
+    }
+
     public historialEntregas()
     {
         InitializeComponent();
+        NavigationPage.SetHasNavigationBar(this, false);
+        BindingContext = this;
 
         // Inicia objetos de prueba para collection view
-        Entregas = new ObservableCollection<EntregaItem>
-            {
-                new EntregaItem { ImageSource = "Productos/flowers.png", LabelCodigoPedido = "#PED-56546", LabelFechaEntrega = "2024-02-01", LabelComentario = "El Repartidor Llego Tarde Esta es una prueba de cantidad de texto en el item!", TappedCommand = new Command(() => HandleItemTapped("Item 1")) },
-                new EntregaItem { ImageSource = "Productos/flowers.png", LabelCodigoPedido = "#PED-56546", LabelFechaEntrega = "2024-02-01", LabelComentario = "El Pedido ha llegado bien!", TappedCommand = new Command(() => HandleItemTapped("Item 2")) },
-                new EntregaItem { ImageSource = "Productos/flowers.png", LabelCodigoPedido = "#PED-56546", LabelFechaEntrega = "2024-02-01", LabelComentario = "El Repartidor Llego Tarde!", TappedCommand = new Command(() => HandleItemTapped("Item 3")) },
-                new EntregaItem { ImageSource = "Productos/flowers.png", LabelCodigoPedido = "#PED-56546", LabelFechaEntrega = "2024-02-01", LabelComentario = "El Pedido ha llegado bien!", TappedCommand = new Command(() => HandleItemTapped("Item 4")) },
-                new EntregaItem { ImageSource = "Productos/flowers.png", LabelCodigoPedido = "#PED-56546", LabelFechaEntrega = "2024-02-01", LabelComentario = "El Repartidor Llego Tarde!", TappedCommand = new Command(() => HandleItemTapped("Item 5")) },
-                new EntregaItem { ImageSource = "Productos/flowers.png", LabelCodigoPedido = "#PED-56546", LabelFechaEntrega = "2024-02-01", LabelComentario = "El Pedido ha llegado bien!", TappedCommand = new Command(() => HandleItemTapped("Item 6")) },
-            };
 
         filtros = new ObservableCollection<string>
             {
                 "Ninguno",
+                "Hoy",
                 "Esta Semana",
-                "Esta Quincena",
                 "Este Mes",
                 "Este Año",
         };
 
-        collectionViewEntregas.ItemsSource = Entregas;
         filtroPicker.ItemsSource = filtros;
     }
 
-    public class EntregaItem
+    protected override async void OnAppearing()
     {
-        public string ImageSource { get; set; }
-        public string LabelCodigoPedido { get; set; }
-        public string LabelFechaEntrega { get; set; }
-        public string LabelComentario { get; set; }
-        public ICommand TappedCommand { get; set; }
-    }
+        base.OnAppearing();
 
-    private void HandleItemTapped(string itemName)
-    {
-        // Handle the tapped item
+        try
+        {
+            var historial = await _apiService.PostDataAsync<historialEntregasModel[]>("historialEntregas.php", new { idrepartidor = 4 });
+            Historiales = new ObservableCollection<historialEntregasViewModel>();
+
+            foreach (var item in historial)
+            {
+                string? image = null;
+                string? estado = null;
+                Color? color = null;
+
+                string? comentario = null;
+
+                //variables imagenes
+                string? star1 = null;
+                string? star2 = null;
+                string? star3 = null;
+                string? star4 = null;
+                string? star5 = null;
+
+                if (string.IsNullOrEmpty(item.motivocalificacion))
+                {
+                    comentario = "El cliente aun no deja una calificación";
+                    star1 = "ImagenesCalificacion/estrella_vacia.png";
+                    star2 = "ImagenesCalificacion/estrella_vacia.png";
+                    star3 = "ImagenesCalificacion/estrella_vacia.png";
+                    star4 = "ImagenesCalificacion/estrella_vacia.png";
+                    star5 = "ImagenesCalificacion/estrella_vacia.png";
+                }
+                else
+                {
+                    comentario = item.motivocalificacion;
+
+                    string[] stars = new string[5];
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (item.calificacion > i)
+                        {
+                            stars[i] = "ImagenesCalificacion/estrella_llena1.png";
+                        }
+                        else
+                        {
+                            stars[i] = "ImagenesCalificacion/estrella_vacia.png";
+                        }
+                    }
+
+                    star1 = stars[0];
+                    star2 = stars[1];
+                    star3 = stars[2];
+                    star4 = stars[3];
+                    star5 = stars[4];
+                }
+
+
+                if (item.idestadopedido == 4)
+                {
+                    image = "Estados/delivery.jpg";
+                    estado = "Entregado";
+                    color = Color.FromRgb(0, 128, 0);
+                }
+                else if (item.idestadopedido == 5)
+                {
+                    image = "Estados/cancelado.jpg";
+                    estado = "Cancelado";
+                    color = Color.FromRgb(255, 0, 0);
+                }
+
+                string? fechaPedidoFormateada = (item.fechapedido).ToString("d/M/yyyy h:mm tt");
+
+                var historialItem = new historialEntregasViewModel
+                {
+                    IdItem = $"Pedido: #PED-{item.idpedido}",
+                    FechaPedido = item.fechapedido,
+                    NombresCliente = $"Nombre Cliente: {item.nombrescliente}",
+                    Ganancia = item.ganancia,
+                    EstadoPedido = $"Estado: {estado}",
+                    Calificacion = item.calificacion,
+                    MotivoCalificacion = $"Comentario: {comentario}",
+                    FechaPedidoFormateada = fechaPedidoFormateada,
+                    FrameBackgroundColor = color,
+                    TextColor = color,
+                    ImageSource = image,
+                    ImageStar1 = star1,
+                    ImageStar2 = star2,
+                    ImageStar3 = star3,
+                    ImageStar4 = star4,
+                    ImageStar5 = star5,
+                    Visibilidad = true
+                };
+
+                Historiales.Add(historialItem);
+            }
+
+            collectionViewEntregas.ItemsSource = Historiales;
+        }
+        catch (Exception ex)
+        {
+            Historiales = new ObservableCollection<historialEntregasViewModel>();
+
+            var historialItem = new historialEntregasViewModel
+            {
+                IdItem = "¡Atención!",
+                ImageSource = "Estados/empty.png",
+                Visibilidad = false,
+                MotivoCalificacion = "No existe un historial de Pedidos",
+                FrameBackgroundColor = Color.FromRgb(255, 0, 0)
+        };
+
+            Historiales.Add(historialItem);
+
+            filtroPicker.IsEnabled = false;
+            searchBarEntregas.IsEnabled = false;
+            searchBarEntregas.IsReadOnly = true;
+
+            collectionViewEntregas.ItemsSource = Historiales;
+        }
+        
     }
 
     private void btnAtras_Clicked(object sender, EventArgs e)
     {
-
+        Navigation.PopAsync();
     }
 
     private void btnNotification_Clicked(object sender, EventArgs e)
@@ -61,12 +203,65 @@ public partial class historialEntregas : ContentPage
 
     private void searchBarEntregas_SearchButtonPressed(object sender, EventArgs e)
     {
-
+        SearchQuery = searchBarEntregas.Text;
     }
 
     private void searchBarEntregas_TextChanged(object sender, TextChangedEventArgs e)
     {
+        if (string.IsNullOrWhiteSpace(e.NewTextValue))
+        {
+            collectionViewEntregas.ItemsSource = Historiales;
+        }
+        else
+        {
+            SearchQuery = e.NewTextValue;
+        }
+    }
 
+    private void UpdateFilteredItems()
+    {
+        // Get the start and end dates based on the selected filter
+        DateTime startDate;
+        DateTime endDate = DateTime.Now;
+
+        switch (filtroPicker.SelectedIndex)
+        {
+            case 1: // Today
+                startDate = DateTime.Today;
+                break;
+            case 2: // This week
+                startDate = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek);
+                break;
+            case 3: // This month
+                startDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+                break;
+            case 4: // This year
+                startDate = new DateTime(DateTime.Today.Year, 1, 1);
+                break;
+            default:
+                // No filter selected
+                startDate = DateTime.MinValue; // Set a default value
+                break;
+        }
+
+        // Filter items based on the selected category and search query
+        var filteredItems = Historiales.Where(item =>
+            (SelectedFilter == null || SelectedFilter == -1 || // Check if no filter selected
+            (item.FechaPedido >= startDate && item.FechaPedido <= endDate)) &&
+            (string.IsNullOrEmpty(SearchQuery) ||
+            item.IdItem.ToLower().Contains(SearchQuery.ToLower()) ||
+            item.NombresCliente.ToLower().Contains(SearchQuery.ToLower()))
+        );
+
+        // Show all items when no filter is selected and no search query
+        if (SelectedFilter == null && string.IsNullOrEmpty(SearchQuery))
+        {
+            collectionViewEntregas.ItemsSource = Historiales;
+        }
+        else
+        {
+            collectionViewEntregas.ItemsSource = new ObservableCollection<historialEntregasViewModel>(filteredItems);
+        }
     }
 
     private void btnInicioRepartidor_Clicked(object sender, EventArgs e)
@@ -101,6 +296,7 @@ public partial class historialEntregas : ContentPage
 
     private void filtroPicker_SelectedIndexChanged(object sender, EventArgs e)
     {
-
+        SelectedFilter = filtroPicker.SelectedIndex;
+        UpdateFilteredItems();
     }
 }
